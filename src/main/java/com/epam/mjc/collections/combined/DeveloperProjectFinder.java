6603d1d15fe9643c62bd008f8dadd44ce492caed
TLGG16 <TLGG16@yandex.ru>
2023-06-19 00:13:34 +0300

    kekw

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,22 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> answ = new ArrayList<>();
+        projects.forEach((key,value)->{
+            if (value.contains(developer)) answ.add(key);
+        });
+
+        answ.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length()>o2.length()) return -1;
+                if (o1.length()<o2.length()) return 1;
+                return 0;
+            }
+        });
+        return answ;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,25 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer,Set<String>> answ = new HashMap<>();
+        sourceMap.forEach((key,value)->{
+            if (answ.containsKey(key.length())){
+                Set<String> set = new HashSet<>(  answ.get(key.length()));
+                set.add(key);
+                answ.put(key.length(), set);
+            }else {
+                Set<String> set = new HashSet<>();
+                set.add(key);
+                answ.put(key.length(), set);
+            }
+        });
+
+        return answ;
     }
 }
